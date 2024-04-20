910640c81114d7ac324dda8282cccd6d5e573dc8
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2024-04-20 15:25:16 -0500

    Remove unnecessary providers in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,8 @@
 import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
-import { ThemeModeSwitcherDirective } from '../directives/theme-mode-switcher.directive'
 
-import { SerifSwitcherDirective } from '../directives/serif-switcher.directive'
-import { ThemeSwitcherDirective } from '../directives/theme-switcher.directive'
-import { ThemeService } from '../services/theme.service'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), ThemeModeSwitcherDirective, SerifSwitcherDirective, ThemeSwitcherDirective, ThemeService],
+    providers: [provideRouter(routes)],
 }
