0066754d508391d7d7d5a56f1cd1f96db88529d5
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2024-04-25 14:05:36 -0500

    Sets up a basic way to add content to the template without needing to worry about HTML * Makes the router-outlet-content the _true_ scrollable in which overflow content will be scrolled against. * Adds basic JSON template - needs work but works for now * Updated styles to accommodate the scroll issues in the flexbox  setup.

---
+++ src/models/content.model.ts
@@ -0,0 +1,17 @@
+export interface BlogSection {
+    /** The header of the section */
+    header: string;
+    /** An array of paragraphs that make up the section */
+    paragraphs: string[];
+}
+
+/**
+ * Content for blog type posts
+ */
+export interface BlogContent {
+    /** The header of the blog post */
+    title: string;
+    /** An array of sections (headers & paragraphs) that make up the blog post */
+    sections: BlogSection[];
+}
+
