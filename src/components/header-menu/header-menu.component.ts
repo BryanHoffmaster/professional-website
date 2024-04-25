72ae866fde1b500f20b9cb46a9eafb0bbb8a12cb
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2024-04-25 11:43:58 -0500

    Add basic routing via "a" tag links in header

--- src/components/header-menu/header-menu.component.ts
+++ src/components/header-menu/header-menu.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header-menu.component.html',
   styleUrl: './header-menu.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
